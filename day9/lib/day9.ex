1bb6d79a7100f3a6b789de96ae5224e77cc4952d
Matt Campbell <matt@soupmatt.com>
2018-12-10 12:44:26 -0600

    Make solution for day 9 puzzle 1 much much faster to solve puzzle 2

--- day9/lib/day9.ex
+++ day9/lib/day9.ex
@@ -73,50 +73,133 @@ defmodule Day9 do
 
   @spec run_game(pos_integer, pos_integer) :: {players, board}
   def run_game(num_players, max_marble) when num_players >= 1 and max_marble >= 1 do
-    run_game(List.duplicate(0, num_players), [], [0], 1, max_marble)
+    run_game(List.duplicate(0, num_players), [], [], [0], 1, max_marble)
   end
 
   # cycle the players back around
-  @spec run_game(players, players, board, marble, marble) :: {players, board}
-  defp run_game([], players_gone, board, next_marble, max_marble) do
-    run_game(Enum.reverse(players_gone), [], board, next_marble, max_marble)
+  @spec run_game(players, players, board, board, marble, marble) :: {players, board}
+  defp run_game([], players_gone, counter_board, clockwise_board, next_marble, max_marble) do
+    run_game(
+      Enum.reverse(players_gone),
+      [],
+      counter_board,
+      clockwise_board,
+      next_marble,
+      max_marble
+    )
   end
 
   # base case
-  @spec run_game(players, players, board, marble, marble) :: {players, board}
-  defp run_game(players_to_go, players_gone, board, next_marble, max_marble)
+  @spec run_game(players, players, board, board, marble, marble) :: {players, board}
+  defp run_game(
+         players_to_go,
+         players_gone,
+         counter_board,
+         clockwise_board,
+         next_marble,
+         max_marble
+       )
        when next_marble > max_marble do
-    {Enum.reverse(players_gone) ++ players_to_go, board}
+    {Enum.reverse(players_gone, players_to_go), clockwise_board ++ Enum.reverse(counter_board)}
   end
 
-  @spec run_game(players, players, board, marble, marble) :: {players, board}
-  defp run_game([next_player | players_to_go], players_gone, board, next_marble, max_marble) do
-    {board, points_awarded} = add_marble_to_board(board, next_marble)
+  @spec run_game(players, players, board, board, marble, marble) :: {players, board}
+  defp run_game(
+         [next_player | players_to_go],
+         players_gone,
+         counter_board,
+         clockwise_board,
+         next_marble,
+         max_marble
+       ) do
+    {counter_board, clockwise_board, points_awarded} =
+      add_marble_to_board(counter_board, clockwise_board, next_marble)
 
     run_game(
       players_to_go,
       [next_player + points_awarded | players_gone],
-      board,
+      counter_board,
+      clockwise_board,
       next_marble + 1,
       max_marble
     )
   end
 
-  @spec add_marble_to_board(board, marble) :: {board, pos_integer}
-  def add_marble_to_board(board, next_marble) when next_marble < 2 do
-    {[next_marble | board], 0}
+  @spec add_marble_to_board(board, board, marble) :: {board, board, pos_integer}
+  def add_marble_to_board(counter_board, clockwise_board, next_marble) when next_marble <= 2 do
+    {counter_board, [next_marble | clockwise_board], 0}
+  end
+
+  @spec add_marble_to_board(board, board, marble) :: {board, board, pos_integer}
+  def add_marble_to_board(counter_board, [current | []], next_marble) do
+    {counter_board, counter_tail} = split_list(counter_board)
+    add_marble_to_board(counter_board, [current | counter_tail], next_marble)
   end
 
   # scoring turn
-  @spec add_marble_to_board(board, marble) :: {board, pos_integer}
-  def add_marble_to_board(board, next_marble) when rem(next_marble, 23) == 0 do
-    {back, [removed | front]} = Enum.split(board, -7)
-    {front ++ back, next_marble + removed}
+  @spec add_marble_to_board(board, board, marble) :: {board, board, pos_integer}
+  def add_marble_to_board(counter_board, clockwise_board, next_marble)
+      when rem(next_marble, 23) == 0 do
+    {counter_board, clockwise_board, points} = remove_marble(counter_board, clockwise_board, 7)
+    {counter_board, clockwise_board, points + next_marble}
   end
 
   # non-scoring turn
-  @spec add_marble_to_board(board, marble) :: {board, pos_integer}
-  def add_marble_to_board([old_current, one_clockwise | rest], next_marble) do
-    {[next_marble | rest] ++ [old_current, one_clockwise], 0}
+  @spec add_marble_to_board(board, board, marble) :: {board, board, pos_integer}
+  def add_marble_to_board(
+        counter_board,
+        [old_current, one_clockwise | clockwise_board],
+        next_marble
+      ) do
+    {[one_clockwise, old_current | counter_board], [next_marble | clockwise_board], 0}
+  end
+
+  @spec split_list(board) :: {board, board}
+  def split_list([]) do
+    {[], []}
+  end
+
+  @spec split_list(board) :: {board, board}
+  def split_list([elem | []]) do
+    {[], [elem]}
+  end
+
+  @spec split_list(board) :: {board, board}
+  def split_list([e1, e2 | []]) do
+    {[e1 | []], [e2 | []]}
+  end
+
+  @spec split_list(board) :: {board, board}
+  def split_list([e1, e2, e3 | []]) do
+    {[e1, e2 | []], [e3 | []]}
+  end
+
+  @spec split_list(board) :: {board, board}
+  def split_list(board) when length(board) <= 9 do
+    split_list2(board)
+  end
+
+  @spec split_list(board) :: {board, board}
+  def split_list([e1, e2, e3, e4, e5, e6, e7 | board]) do
+    {[e1, e2, e3, e4, e5, e6, e7 | []], Enum.reverse(board)}
+  end
+
+  defp split_list2([e1, e2, e3 | []]) do
+    {[e1], [e3, e2]}
+  end
+
+  defp split_list2([head | board]) do
+    {left, right} = split_list2(board)
+    {[head | left], right}
+  end
+
+  @spec remove_marble(board, board, non_neg_integer) :: {board, board, pos_integer}
+  def remove_marble([c_head | counter_board], clockwise_board, 1) do
+    {counter_board, clockwise_board, c_head}
+  end
+
+  @spec remove_marble(board, board, non_neg_integer) :: {board, board, pos_integer}
+  def remove_marble([c_head | counter_board], clockwise_board, count) do
+    remove_marble(counter_board, [c_head | clockwise_board], count - 1)
   end
 end
--- day9/test/day9_test.exs
+++ day9/test/day9_test.exs
@@ -11,7 +11,7 @@ defmodule Day9Test do
       assert Day9.run_game(1, 2) == {[0], [2, 1, 0]}
       assert Day9.run_game(1, 3) == {[0], [3, 0, 2, 1]}
       assert Day9.run_game(1, 4) == {[0], [4, 2, 1, 3, 0]}
-      assert Day9.run_game(1, 4) == {[0], [4, 2, 1, 3, 0]}
+      assert Day9.run_game(1, 5) == {[0], [5, 1, 3, 0, 4, 2]}
 
       assert Day9.run_game(1, 22) ==
                {[0],
@@ -22,6 +22,58 @@ defmodule Day9Test do
       assert Day9.run_game(1, 23) ==
                {[32],
                 [19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15, 0, 16, 8, 17, 4, 18]}
+    end
+  end
+
+  describe "split_list" do
+    test "base_case" do
+      input = [0]
+      assert Day9.split_list(input) == {[], [0]}
+    end
+
+    test "2 elements" do
+      input = [0, 1]
+      assert Day9.split_list(input) == {[0], [1]}
+    end
+
+    test "3 elements" do
+      input = [0, 1, 2]
+      assert Day9.split_list(input) == {[0, 1], [2]}
+    end
+
+    test "4 elements" do
+      input = [0, 1, 2, 3]
+      assert Day9.split_list(input) == {[0, 1], [3, 2]}
+    end
+
+    test "5 elements" do
+      input = [0, 1, 2, 3, 4]
+      assert Day9.split_list(input) == {[0, 1, 2], [4, 3]}
+    end
+
+    test "6 elements" do
+      input = [0, 1, 2, 3, 4, 5]
+      assert Day9.split_list(input) == {[0, 1, 2, 3], [5, 4]}
+    end
+
+    test "7 elements" do
+      input = [0, 1, 2, 3, 4, 5, 6]
+      assert Day9.split_list(input) == {[0, 1, 2, 3, 4], [6, 5]}
+    end
+
+    test "8 elements" do
+      input = [0, 1, 2, 3, 4, 5, 6, 7]
+      assert Day9.split_list(input) == {[0, 1, 2, 3, 4, 5], [7, 6]}
+    end
+
+    test "9 elements" do
+      input = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      assert Day9.split_list(input) == {[0, 1, 2, 3, 4, 5, 6], [8, 7]}
+    end
+
+    test "10 elements" do
+      input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      assert Day9.split_list(input) == {[0, 1, 2, 3, 4, 5, 6], [9, 8, 7]}
     end
   end
 end
