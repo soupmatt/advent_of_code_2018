c8716368d1cb83151112cc9153ec3298479d9ea8
Matt Campbell <matt@soupmatt.com>
2018-12-20 10:22:20 -0600

    Debugging for day15, still doesn't work :(

--- day15/puzzle1.exs
+++ day15/puzzle1.exs
@@ -1,4 +1,23 @@
+board =
 File.read!("input.txt")
 |> Day15.parse_input()
-|> Day15.fight!()
-|> IO.inspect()
+
+{board, count} = Enum.reduce_while(0..1000, board, fn count, board ->
+  IO.puts "Round #{count} ======================"
+  IO.puts Day15.print_board(board)
+  # Process.sleep(3_000)
+  case Day15.run_round(board) do
+    {:ok, board} ->
+      {:cont, board}
+    {:no_targets, board} ->
+      {:halt, {board, count}}
+  end
+end)
+
+IO.puts "======================="
+IO.puts "Round #{count} was in progress"
+IO.puts Day15.print_board(board)
+
+IO.puts ""
+
+IO.puts Day15.score_fight(board, count)
